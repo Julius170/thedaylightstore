eb2738243687fc387fa555bfff367582fc3f0f6f
kinyichukwu <kinyichukwuose@gmail.com>
2022-12-12 23:41:24 +0100

    still designing

--- designStore/indes.js
+++ designStore/indes.js
@@ -1,3 +1,8 @@
 const bar = document.getElementById('bar')
 const nav = document.getElementById('header__navbar')
 
+if(bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active')
+    })
+}
