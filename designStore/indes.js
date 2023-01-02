eea085f8676ce07afe6938b85906ce2fd2d378c4
kinyichukwu <kinyichukwuose@gmail.com>
2023-01-02 18:29:06 +0100

    Finished design of homepage

--- designStore/indes.js
+++ designStore/indes.js
@@ -1,8 +1,17 @@
-const bar = document.getElementById('bar')
-const nav = document.getElementById('header__navbar')
+const menuButton = document.getElementById('bar')
+const menu = document.getElementById('header__navbar')
+const navLink = document.querySelectorAll('.navbar_links')
 
-if(bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active')
-    })
-}
+menuButton.addEventListener('click', () => {
+    if(menu.classList.contains('show')){
+        menu.classList.remove('show')
+    }else{
+        menu.classList.add('show')
+    }
+    
+})
+
+navLink.forEach( (x) => {
+    x.addEventListener('click', () => menu.classList.contains('show')? menu.classList.remove('show'): 0)
+})
+
