95260a268e2bdc2b052a66b012dc01accbb2eb0e
kinyichukwu <kinyichukwuose@gmail.com>
2023-01-03 01:13:32 +0100

    Sweet, finishing the shop page

--- designStore/indes.js
+++ designStore/indes.js
@@ -1,17 +1,29 @@
-const menuButton = document.getElementById('bar')
-const menu = document.getElementById('header__navbar')
-const navLink = document.querySelectorAll('.navbar_links')
+const menuButton = document.getElementById("bar");
+const menu = document.getElementById("header__navbar");
+const navLink = document.querySelectorAll(".navbar_links");
 
-menuButton.addEventListener('click', () => {
-    if(menu.classList.contains('show')){
-        menu.classList.remove('show')
-    }else{
-        menu.classList.add('show')
-    }
-    
-})
+menuButton.addEventListener("click", () => {
+  if (menu.classList.contains("show")) {
+    menu.classList.remove("show");
+  } else {
+    menu.classList.add("show");
+  }
+});
 
-navLink.forEach( (x) => {
-    x.addEventListener('click', () => menu.classList.contains('show')? menu.classList.remove('show'): 0)
-})
 
+// const removeActive = () => {
+//   navLink.forEach((y) => {
+//     if (x.classList.contains(y)) {
+//       x.classList.remove("active");
+//     }
+//   });
+// };
+
+navLink.forEach((x) => {
+  x.addEventListener("click", () => {
+    menu.classList.contains("show") ? menu.classList.remove("show") : 0;
+    // removeActive();
+
+    // x.classList.add("active");
+  });
+});
