a036755675691d0c4b3c8213b73d10e62830f7f1
kinyichukwu <kinyichukwuose@gmail.com>
2023-01-07 01:03:38 +0100

    3% done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,12 @@
-
-import Home from './routes/home/home';
-import Navigation from './routes/navigation/navigation';
-
+import "./App.scss";
+import Home from "./routes/home/home";
+import Navigation from "./routes/navigation/navigation";
 
 function App() {
   return (
     <div className="App">
-    <Navigation/>
-<Home/>
+      <Navigation />
+      <Home />
     </div>
   );
 }
