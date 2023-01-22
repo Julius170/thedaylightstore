e55e0e8786f3fdfb9b5b9029ae2b1043c3c212e8
kinyichukwu <kinyichukwuose@gmail.com>
2023-01-22 20:10:14 +0100

    ...

--- react-app/src/routes/admin/addProduct.js
+++ react-app/src/routes/admin/addProduct.js
@@ -4,30 +4,50 @@ import FormInput from "../../components/login/form-input/form-input.component";
 import { styleButton } from "../authentication/authentication.component";
 import Navigation from "../navigation/navigation";
 import Button from "../../components/button/button.component";
+import './addProduct.styles.scss'
 
 export const Admin = () => {
   return (
-    <div className="sign-in-container">
-      <h2>Already have an account?</h2>
+    <div className="add-product-to-cart">
+      <h2>Add new product</h2>
       <span>Sign in with your email and password</span>
       <form >
         <FormInput
-          label="Email"
-          type="email"
+          label="productName"
+          type="text"
           required
           onChange={()=>{}}
-          name="email"
-          value="byhy"
+          name="productName"
+          value=""
         />
 
         <FormInput
-          label="Password"
-          type="password"
+          label="productImage"
+          type="text"
           required
           onChange={()=>{}}
-          name="password"
-          value="byhy"
+          name="productImage"
+          value=""
         />
+
+        <FormInput
+          label="productPrice"
+          type="text"
+          required
+          onChange={()=>{}}
+          name="productPrice"
+          value=""
+        />
+
+        <FormInput
+          label="productName"
+          type="text"
+          required
+          onChange={()=>{}}
+          name="productName"
+          value=""
+        />
+
         <div className="buttons-container">
           <Button type="submit" style={styleButton} className="normal">
             Sign In
