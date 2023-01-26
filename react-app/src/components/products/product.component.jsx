0a9344b2d98728510e78ea0ca79f946179d4c185
kinyichukwu <kinyichukwuose@gmail.com>
2023-01-26 10:10:36 +0100

    fixed shop data

--- react-app/src/components/products/product.component.jsx
+++ react-app/src/components/products/product.component.jsx
@@ -6,16 +6,22 @@ const Product = () => {
   const { categoriesMap } = useContext(CategoriesContext);
   console.log(categoriesMap, "this is it guys");
   const { hats, jackets, mens, women, sneakers } = categoriesMap;
+
   return (
     <section id="product1" className="section-p1">
       <h2>Featured Products</h2>
       <p>Summer Collection New Morden Design</p>
       <div className="pro-container">
-        {mens &&
-          mens.map((x, i) => {
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          products.map((x, i) => {
             const { id } = x;
-            return <ProductItem key={id} product={x} group="shirts" />;
-          })}
+            return <ProductItem key={id} product={x} group={title} />;
+          })
+        );
+      })}
+       
       </div>
     </section>
   );
