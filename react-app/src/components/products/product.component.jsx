9ef2f3187e9b63dfc9048d2a19e5d89d5abfa91a
kinyichukwu <kinyichukwuose@gmail.com>
2023-01-28 18:14:43 +0100

    ...

--- react-app/src/components/products/product.component.jsx
+++ react-app/src/components/products/product.component.jsx
@@ -1,28 +1,27 @@
 import ProductItem from "../product-item/product-item.component";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CategoriesContext } from "../../contexts/categories.context";
 
-const Product = ({valueProp}) => {
+const Product = ({ valueProp }) => {
   const { categoriesMap } = useContext(CategoriesContext);
-  
 
   return (
     <section id="product1" className="section-p1">
-      
       <div className="pro-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          products.map((x) => {
-            const { id } = x;
-            
-            return <ProductItem key={id} product={x} group={title} />;
-          })
-        );
-      })}
+        {Object.keys(categoriesMap).map((title) => {
+          const products = categoriesMap[title];
+          return products
+            .filter((x) => {
+              return x.name
+                .toLocaleLowerCase()
+                .includes(valueProp.toLocaleLowerCase());
+            })
+            .map((x) => {
+              const { id } = x;
 
-      
-       
+              return <ProductItem key={id} product={x} group={title} />;
+            });
+        })}
       </div>
     </section>
   );
